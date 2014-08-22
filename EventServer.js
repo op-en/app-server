75cccdf7a28998db4f2cf55b476517f21bd14de4
Anton04 <anton.gustafsson@tii.se>
2014-08-22 22:42:19 +0200

    Update EventServer.js

--- EventServer.js
+++ EventServer.js
@@ -1,13 +1,18 @@
 var sys = require('sys');
 var net = require('net');
 //var mqtt = require('mqtt');
+var mqtt = require('mqtt');
 
 var io  = require('socket.io').listen(5000);
 
+io.on('connection', function(socket){
+  console.log('a user connected');
+  socket.on('disconnect', function(){
+    console.log('user disconnected');
+  });
+});
 
-
-var mqtt = require('mqtt')
-  , client = mqtt.createClient();
+client = mqtt.createClient();
 
 client.subscribe('#');
 //client.publish('messages', 'hello me!');
