5abc5c6a9954f3fc905e12d12fc471ea10a0dc0f
gustafsson <anton.gustafsson@tii.se>
2014-08-22 23:03:13 +0200

    Added uniqe instance for each connection to socket.io towards mqtt to get retained messages

--- EventServer.js
+++ EventServer.js
@@ -7,16 +7,26 @@ var io  = require('socket.io').listen(5000);
 
 io.on('connection', function(socket){
   console.log('a user connected');
+
+  socket.mqtt = mqtt.createClient();
+  socket.mqtt.subscribe('#');
+
+  //Forward all messages to socket.io
+  socket.mqtt.on('message', function(topic, message) {
+    console.log(message);
+    io.sockets.emit('mqtt',{'topic':String(topic),'payload':String(message)});
+  });
+
   socket.on('disconnect', function(){
     console.log('user disconnected');
   });
 });
 
-client = mqtt.createClient();
+//client = mqtt.createClient();
 
-client.subscribe('#');
+//client.subscribe('#');
 //client.publish('messages', 'hello me!');
-client.on('message', function(topic, message) {
-  console.log(message);
-  io.sockets.emit('mqtt',{'topic':String(topic),'payload':String(message)});
-});
+//client.on('message', function(topic, message) {
+//  console.log(message);
+//  io.sockets.emit('mqtt',{'topic':String(topic),'payload':String(message)});
+//});
