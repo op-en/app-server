8c0ba2479e2359036cd26cac8fd2de2cef175f13
Anton04 <anton.gustafsson@tii.se>
2015-10-15 19:08:44 +0200

    Update install.sh

--- install.sh
+++ install.sh
@@ -5,5 +5,12 @@ echo "Installing AppServer"
 mkdir ~/repos
 cd ~/repos
 git clone https://github.com/Anton04/AppServer.git
+cd ~/repos/AppServer
+
+. ~/.nvm/nvm.sh
+nvm use 0.12
+
+npm install socket.io mqtt
+
 
 
